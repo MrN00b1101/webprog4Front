803812d3ff9ffc0cfa4b9ed3f9de94fd09110517
MrNoob1101 <vargabalazs900513@gmail.com>
2019-11-26 20:34:50 +0100

    api service

--- webProg4Front/src/app/api.service.ts
+++ webProg4Front/src/app/api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 
 @Injectable({
@@ -8,9 +8,23 @@ import { HttpClient,HttpHeaders } from '@angular/common/http';
 
 export class DataService {
 
-  private url= "http://localhost:8080";
+  private url = 'http://localhost:8080';
 
   constructor(private httpClient: HttpClient) { }
-
-
+  public getLogin(obj) {
+    return this.httpClient.get(this.url + '/login', obj);
+  }
+  public getTranList() {
+    return this.httpClient.get(this.url + '/transactions');
+  }
+  public postTran(obj) {
+    return this.httpClient.post(this.url + '/transactions', obj, {
+      headers: new HttpHeaders({'Content-Type':  'application/json', })
+    });
+  }
+  public postRegister(obj) {
+    return this.httpClient.post(this.url + '/register', obj, {
+      headers: new HttpHeaders({'Content-Type':  'application/json', })
+    });
+  }
 }
